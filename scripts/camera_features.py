97ade049bc76cf9f36ff5cd916162ca9c3fba268
jmacglashan <jmacglashan@gmail.com>
2015-11-05 18:11:03 -0500

    modified script

--- scripts/camera_features.py
+++ scripts/camera_features.py
@@ -35,7 +35,7 @@ class CameraFeatures:
 			cv2.imwrite('cr.jpg', cr)
 			cv2.imwrite('cb.jpg', cb)
 
-			di = self.distImage(conv, (100, 255, 64), (1,0,0))
+			di = self.distImage(conv, (0, 255, 64), (1,0,0))
 			print 'writing dist image'
 			cv2.imwrite('bdist.jpg', di)
 			cv2.imwrite('extract.jpg', cv_image)
