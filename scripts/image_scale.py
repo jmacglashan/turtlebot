b9cb6cfe24da4cfa09bc7d986fd49b47992e2e32
jmacglashan <jmacglashan@gmail.com>
2016-03-29 18:23:53 -0400

    added image scale

---
+++ scripts/image_scale.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from turtle_env.msg import CameraFeatures
+from turtle_env.msg import CameraScaleFeatures
+from turtle_env.msg import CameraChannels
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import math
+import time
+
+class ImageScale:
+
+    def __init__(self):
+        self.nPrints = 1
+        self.bridge = CvBridge()
+        rospy.init_node('camera_scaled', anonymous=True)
+        rospy.Subscriber('camera/rgb/image_color', Image, self.cameraCallback, queue_size=1)
+        self.image_pub = rospy.Publisher("turtle_env/camera_scaled", Image)
+
+    def cameraCallback(self, data):
+        if self.nPrints > 0:
+            print 'getting callback'
+            self.nPrints -= 1
+
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        except CvBridge, e:
+            print e
+
+        cv_image = cv2.resize(cv_image, (160, 120))
+        try:
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cdi, "rgb8"))
+        except CvBridgeError, e:
+            print e
+
+
